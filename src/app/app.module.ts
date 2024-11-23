54566651902c061a91c54ceb38310ad8b8b15ed9
annacoutinho <annacarol.coutinho@icloud.com>
2024-11-23 17:25:17 -0300

    feat: criacao do componente button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SocialsButtonComponent } from './shared/components/socials-button/socials-button.component';
 import { DropdownComponent } from './shared/components/dropdown/dropdown.component';
+import { ButtonComponent } from './shared/components/button/button.component';
 
 @NgModule({
-  declarations: [AppComponent, SocialsButtonComponent, DropdownComponent],
+  declarations: [AppComponent, SocialsButtonComponent, DropdownComponent, ButtonComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
