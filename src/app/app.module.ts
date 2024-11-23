44a07e8a2b46a807267db15903c52b17293eb378
annacoutinho <annacarol.coutinho@icloud.com>
2024-11-23 16:54:56 -0300

    feat: criacao do componente de botoes das redes sociais

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SocialsButtonComponent } from './shared/components/socials-button/socials-button.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SocialsButtonComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
