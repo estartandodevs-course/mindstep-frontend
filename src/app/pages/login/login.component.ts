c6a42c5f311a650fbbd40510ed3d42735af71548
annacoutinho <annacarol.coutinho@icloud.com>
2024-11-24 16:26:41 -0300

    feat: criacao componente login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss',
+  styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+
+  constructor(private router: Router) {}
+
+  onLogin() {
+    console.log('Login:', this.email, this.password);
+    // Adicionar lógica de autenticação
+  }
+
+  navigateToSignup() {
+    this.router.navigate(['/signup']); // Rota para a tela de cadastro
+  }
+}
