be81a71c5df4a3ac5940d85fd94f55c40c83c315
Igor Monsores <igor_monsores@hotmail.com>
2024-12-04 19:03:50 -0300

    feat: atualização de rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+//import { HomeComponent } from './pages/home/home.component';
+
+import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeModule } from './pages/home/home.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, HomeModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
